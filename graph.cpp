295b6ba6af053583eadc204f8b2d3fc46ad34197
Caleb Jones <bionicle.dragon@gmail.com>
2014-01-25 21:43:48 -0500

    Fixed the viewport size bug 	(init() was being called before the file was read) Made code compile on mac Made makefile cross platform Added *.exe and *.DS_Store to the gitignore

--- graph.cpp
+++ graph.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <stdio.h>
+#ifdef	_WIN32
 #include <SDL/SDL.h>
 #include <SDL/SDL_opengl.h>
+#else
+#include <SDL.h>
+#include <SDL_opengl.h>
+#endif
 #include <vector>
-#undef main
 
 using namespace std;
 
@@ -29,16 +33,14 @@ float floatBeingSearchedFor;
 
 float maxPoint;
 
-int main(){
+int main(int argc, char* argv[]){
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Surface *screen = SDL_SetVideoMode(width, height, 32, SDL_OPENGL | SDL_HWSURFACE);
 	SDL_WM_SetCaption("Graph", NULL);
-	
-	init();
-	
+
 	FILE * pFile;
 	pFile=fopen ("data.txt","r");
-	if(pFile==NULL){ 
+	if(pFile==NULL){
 		perror ("Error opening file");
 	}
 
@@ -52,20 +54,22 @@ int main(){
 			cout << "Error reading file." << endl;
 		}
 	}
-	
+
 	fclose (pFile);
-	
+
 	cout << "Finished reading opened file." << endl;
-	
+
 	for(int i = 0; i < points.size(); ++i){
 		cout << points[i] << endl;
 	}
 	//cout << "Max point is set to: " << maxPoint << endl;
 	//cout << "Points length is set to: " << points.size() << endl;
-	
+	init();
+
 	while(running){
 		update();
 	}
+	return 0;
 }
 
 void lockFPS(){
@@ -79,7 +83,7 @@ void init(){
 	glLoadIdentity();
 	cout << "Max point is set to: " << maxPoint << endl;
 	cout << "Points length is set to: " << points.size() << endl;
-	glOrtho(0, width, 0, height	, 0, 1);
+	glOrtho(0, points.size(), 0, maxPoint, 0, 1);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
@@ -104,12 +108,12 @@ void events(){
 
 void render(){
 	glClear(GL_COLOR_BUFFER_BIT);
-	
+
 	glBegin(GL_LINE_STRIP);
 		for(int i = 0; i < points.size(); ++i){
-			glVertex2f(i * 2, points[i]);
+			glVertex2f(i, points[i]);
 		}
 	glEnd();
-	
+
 	SDL_GL_SwapBuffers();
 }
--- makefile
+++ makefile
@@ -1,15 +1,21 @@
+UNAME = $(shell uname)
 # The name of the executable to produce
 EXE = graph.exe
 # The objects to link
 OBJS = graph.o
 
 # Compiler flags:
+ifeq ($(UNAME), $(filter $(UNAME), Linux Darwin))
+LIBS = $(shell sdl-config --static-libs)
+FLAGS = $(shell sdl-config --cflags)
+else
 LIBS = -lmingw32 -lSDLmain -lSDL -lopengl32 -static-libgcc -static-libstdc++
+endif
 
 # Put it all together and compile to .os:
 $(EXE) : $(OBJS)
-	g++ -o $(EXE) $(OBJS) $(LIBS)
+	g++ -o $(EXE) $(OBJS) $(LIBS) $(FLAGS)
 
 # Compile all .cpps into .os:
 %.o : %.cpp
-	g++ $< -c
+	g++ $< -c $(FLAGS)
